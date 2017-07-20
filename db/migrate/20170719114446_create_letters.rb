e3f72a311c51d82a288bf79cdc4500f6d928d544
Dima <naumkindp@mail.ru>
2017-07-20 15:18:53 +0300

    Fix indexes in migrations

--- db/migrate/20170719114446_create_letters.rb
+++ db/migrate/20170719114446_create_letters.rb
@@ -1,13 +1,11 @@
 class CreateLetters < ActiveRecord::Migration
   def change
     create_table :letters do |t|
-      t.string :url
-      t.string :email
-      t.string :mail_status
+      t.string :url, index: true
+      t.string :email, index: true
+      t.string :mail_status, index: true
       t.string :comment
-
       t.timestamps null: false
     end
-    add_index :letters, [:created_at, :mail_status]
   end
 end
