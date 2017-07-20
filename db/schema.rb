b775be09b04d9459c5e779161cf515e76e6669f7
Dima <naumkindp@mail.ru>
2017-07-20 17:41:30 +0300

    Added devise views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170719114446) do
+ActiveRecord::Schema.define(version: 20170720142836) do
 
   create_table "letters", force: :cascade do |t|
     t.string   "url"
@@ -25,5 +25,23 @@ ActiveRecord::Schema.define(version: 20170719114446) do
   add_index "letters", ["email"], name: "index_letters_on_email"
   add_index "letters", ["mail_status"], name: "index_letters_on_mail_status"
   add_index "letters", ["url"], name: "index_letters_on_url"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
